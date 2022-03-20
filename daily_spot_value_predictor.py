e0aec3e19b1a3b39521b90061095d1c2aa57cfab
jtrager <jtrager@umich.edu>
2022-03-19 22:49:47 -0400

    spot predictor [case 1]

--- daily_spot_value_predictor.py
+++ daily_spot_value_predictor.py
@@ -1,16 +1,22 @@
+from sklearn import svm
 import sklearn as sk
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # read in data
 prices_16 = pd.read_csv('prices_2016.csv')
 rain_16 = pd.read_csv('rain_2016.csv')
 
+prices_17 = pd.read_csv('prices_2017.csv')
+rain_17 = pd.read_csv('rain_2017.csv')
+
+
 # input is day of year, precip of that month
 # set up input and ouput
 
 
-dayOfYear = range(251)
+dayOfYear = range(252)
 precip = [] # precipitation for the month that each day is in
 
 for p in rain_16.iloc[:,1]:
@@ -19,7 +25,7 @@ for p in rain_16.iloc[:,1]:
 
 
 
-X = np.column_stack(dayOfYear, precip)
+X = np.column_stack((dayOfYear, precip))
 y = prices_16.iloc[:,1]
 
 # set up model and fit data to model
@@ -28,5 +34,18 @@ regr.fit(X, y)
 
 
 # test output
-test = []
-print("Test Output:",regr.predict(test))
+precip_17 = []
+for p in rain_17.iloc[:,1]:
+    for i in range(21):
+        precip_17.append(p)
+
+
+test = np.column_stack((dayOfYear, precip_17))
+out = regr.predict(test)
+#print("Test Output:",out)
+
+plt.plot(dayOfYear,prices_17.iloc[:,1])
+plt.plot(dayOfYear,out)
+
+#plt.set_label("Ground Truth", "Predict")
+plt.show()
