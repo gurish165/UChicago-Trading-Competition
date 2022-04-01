935f1f32cb35bc1c37bf985392c7cfc6eb45c4d2
jtrager <jtrager@umich.edu>
2022-04-01 03:47:12 -0400

    we really tried rl

--- Case 3/rl_agent.py
+++ Case 3/rl_agent.py
@@ -3,44 +3,69 @@ from gym import spaces
 
 import numpy as np
 
-
-def get_reward(action, price_df):
-    # TODO make reward function
-    # process action
-    return -1
-
-
-
 class AllocateEnv(gym.Env):
-    def __init__(self,window_size):
+    def __init__(self,window_size, price,a_pred1,a_pred2,a_pred3):
         np.random.seed(0)
         self.np_random = np.random
         self.window_size = window_size
-        # allocation of each stock (processed later)
-        self.action_space = spaces.Box(shape=(9,))
-        # analyst 1, analyst 2, analyst 3, window price data
-        self.observation_space = spaces.Tuple((spaces.Box(shape=(9,1)), spaces.Box(shape=(9,1)),
-                                               spaces.Box(shape=(9,1)), spaces.Box(shape=(9,window_width))))
-        self.ts = 0 # timestep
+        self.observation_high_1d = np.full((9,), np.finfo(np.float32).max, dtype=np.float32)
+        self.observation_min_1d = np.full((9,), np.finfo(np.float32).min, dtype=np.float32)
 
-    def step(self, action, price_df,a_df1,a_df2,a_df3):
+        self.observation_high_2d = np.full((9,window_size), np.finfo(np.float32).max, dtype=np.float32)
+        self.observation_min_2d = np.full((9,window_size), np.finfo(np.float32).min, dtype=np.float32)
+
+        # allocation of each stock (processed later)
+        self.action_space = spaces.Box(self.observation_min_1d, self.observation_high_1d, dtype=np.float32)
+        # analyst 1, analyst 2, analyst 3, window price data
+        self.observation_space = spaces.Tuple((spaces.Box(self.observation_min_1d, self.observation_high_1d, dtype=np.float32), spaces.Box(self.observation_min_1d, self.observation_high_1d, dtype=np.float32),
+                                               spaces.Box(self.observation_min_1d, self.observation_high_1d, dtype=np.float32), spaces.Box(self.observation_min_2d, self.observation_high_2d, dtype=np.float32)))
+        self.ts = self.window_size # timestep
+
+        self.price = price
+        self.a_pred1 = a_pred1
+        self.a_pred2 = a_pred2
+        self.a_pred3 = a_pred3
+
+        self.obs_begin = (self.a_pred1[self.ts][:], self.a_pred2[self.ts][:], self.a_pred3[self.ts][:],
+                          self.price[self.ts-self.window_size:self.ts])
+
+        self.returns = []
+
+    def get_reward(self, action):
+        # TODO make reward function
+        # process action
+        weight = action
+        prev_row = np.array(self.price[self.ts], dtype=np.float32)
+        current_row = np.array(self.price[self.ts+1], dtype=np.float32)
+        return_pct = weight*((current_row - prev_row) / prev_row)
+        return np.sum(return_pct, dtype=np.float32)
+
+    def step(self, action):
 
         # set reward based on profit
-        reward = get_reward(action, price_df)
-
-        observation = (a_df1[:][self.ts], a_df2[:][self.ts], a_df3[:][self.ts]) # set observation
-
+        reward = self.get_reward(action)
+        # set observation
+        observation = (np.array(self.a_pred1[self.ts], dtype=np.float32), np.array(self.a_pred2[self.ts], dtype=np.float32),
+                        np.array(self.a_pred3[self.ts][:], dtype=np.float32),
+                       np.array(self.price[self.ts-self.window_size:self.ts], dtype=np.float32))
         self.ts += 1
-
-        if (self.ts >= len(price_df)):
+        done = False
+        if (self.ts >= len(self.price)-1):
             done = True
 
+        self.returns.append(reward)
         return observation, reward, done, {}
 
     def reset(self):
-
-        return (0, 0, False)
+        self.ts = self.window_size
+        self.returns = []
+        return (np.zeros((9,), dtype=np.float32), np.zeros((9,), dtype=np.float32), np.zeros((9,), dtype=np.float32), np.zeros((9,self.window_size), dtype=np.float32))
 
     def render(self):
+        self.returns = np.array(self.returns, dtype=np.float32)
+        print("Mean :",np.mean(self.returns))
+        print("Std :", np.std(self.returns))
+        sharpe_ratio = np.mean(self.returns) / np.std(self.returns)
+        print(f"SHARPE RATIO: {sharpe_ratio}")
         pass
 
--- Case 3/rl_sim.py
+++ Case 3/rl_sim.py
@@ -2,18 +2,18 @@ from rl_agent import AllocateEnv
 import pandas as pd
 import numpy as np
 
-env = AllocateEnv(10)
+price = pd.read_csv('Acutal Testing Data.csv',index_col=0).to_numpy()
+a1 = pd.read_csv('Predicted Testing Data Analyst 1.csv',index_col=0).to_numpy()
+a2 = pd.read_csv('Predicted Testing Data Analyst 2.csv',index_col=0).to_numpy()
+a3 = pd.read_csv('Predicted Testing Data Analyst 3.csv',index_col=0).to_numpy()
 
-price_df = pd.read_csv('Acutal Testing Data.csv').to_numpy()
-a_df1 = pd.read_csv('Predicted Testing Data Analyst 1.csv').to_numpy()
-a_df2 = pd.read_csv('Predicted Testing Data Analyst 2.csv').to_numpy()
-a_df3 = pd.read_csv('Predicted Testing Data Analyst 3.csv').to_numpy()
+env = AllocateEnv(10, price, a1, a2, a3)
 
 while not input("Press enter to continue"):
     observation = env.reset()
     done = False
     while not done:
         action = env.action_space.sample()
-        observation, reward, done, info = env.step(action, price_df,a_df1,a_df2,a_df3)
+        observation, reward, done, info = env.step(action)
     env.render()
 env.close()
